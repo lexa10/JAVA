add0e1acebc5a9b0458b95ee3be38fee33834c8b
adip564rene <adidaslexa@mail.ru>
2019-02-14 22:08:40 +0300

    Не хватило времени!

---
+++ task1/src/main/java/MAinApp.java
@@ -0,0 +1,15 @@
+public class MAinApp {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[100000] ;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.println(arr[i]);
+
+        }
+        }
+    }
+
+
