76f9e5629534475a6f8095baf95f6340aec20b20
adip564rene <adidaslexa@mail.ru>
2019-02-17 15:25:08 +0300

    Добавил остальные задания!

--- task1/src/main/java/Array.java
+++ task1/src/main/java/Array.java
@@ -1,60 +1,55 @@
-import java.util.Arrays;
+ public class Array  {
 
-public class Array<E> {
+     private int size;
+     private int[] arr;
+     private Comparable<Integer>[] data;
 
-    private static final int DEFAULT_CAPACITY = 16;
-    protected static final int INVALID_INDEX = -1;
+     public Array(int size) {
+         this.size = 100000;
+         this.arr = new int[size];
+     }
 
-    protected int currentSize;
-    protected E[] data;
-    private int size;
-
-    public void remove(int index) {
-        boolean result = removeByIndex(index);
-
-    }
-
-    private boolean removeByIndex(int index) {
-        if (index == INVALID_INDEX || index < 0 || index >= currentSize)
-
-        return false;
-        for (int i = index; i < currentSize -1 ; i++) {
-            data[i] = data[i +1];
-        }
-        data[currentSize - 1] = null;
-        currentSize--;
-        return true;
-    }
-
-    public void add(E value) {
-        checkGrow();
-
-        int index;
-
-        for (index = 0; index < currentSize; index++) {
-            if (data[index].compareTo(value) > 0)
-                break;
-        }
-
-        for (int i = currentSize; i > index; i--) {
-            data[i] = data[i - 1];
-        }
-
-        data[index] = value;
-        currentSize++;
-    }
+     public boolean find(int value) {
+         int i;
+         for (i = 0; i < this.size; i++) {
+             if (this.arr[i] == value) {
+                 break;
+             }
+         }
+         if (i == this.size)
+             return false;
+         else
+             return true;
+     }
 
 
-    public boolean find(int value){
-        int i;
-        for(i=0;i<this.size;i++){
-            if (this.arr[i] == value) break;
-        }
-        if (i==this.size)
-            return false;
-        else
-            return true;
-    }
+     public void remove(int value) {
+         int i = 0;
+         for (i = 0; i < this.size; i++) {
+             if (this.arr[i] == value)
+                 break;
+         }
+         for (int j = i; j < this.size - 1; j++) {
+             this.arr[j] = this.arr[j + i];
+         }
+         this.size--;
 
-}
+     }
+
+     public void add(int value) {
+
+         int i = 0;
+
+         for (i = 0; i < this.size; i++) {
+             if (data[i].compareTo(value) > 0)
+                 break;
+         }
+
+         for (i = this.size; i > i; i--) {
+             data[i] = data[i - 1];
+         }
+         data[i] = value;
+         this.size++;
+     }
+ }
 
--- task1/src/main/java/MAinApp.java
+++ task1/src/main/java/MAinApp.java
@@ -1,15 +1,22 @@
 public class MAinApp {
 
+
+
     public static void main(String[] args) {
+
 
         int[] arr = new int[100000] ;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) { // создать массив большого размера
             arr[i] = i;
             System.out.println(arr[i]);
-
         }
+
+        for (int i =0; i < arr.length; i++){ // заполнить массив случайными числами
+            arr[i] = (int)(Math.random()*100000);
+            System.out.println(arr[i]);
+        }
+
         }
     }
-
 
